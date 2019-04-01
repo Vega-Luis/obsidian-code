ea8723d381b8713fd35716df8789249ad3b23333
Marcos Méndez Hidalgo <marcosmh0199@gmail.com>
2019-03-31 22:57:47 -0600

    feat: Vehicles object persistence
    Added methods to save and load vehicles from a .json

--- rent-a-car/src/main/java/persistence/Persistence.java
+++ rent-a-car/src/main/java/persistence/Persistence.java
@@ -1,22 +1,29 @@
 package persistence;
 
 import bussineslogic.Client;
+import bussineslogic.Maintenance;
+import bussineslogic.Vehicle;
 import bussineslogic.Address;
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.image.RenderedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DateFormat.Field;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
+import java.util.List;
 import javax.imageio.ImageIO;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * Clase para la persistencia de datos
@@ -97,9 +104,9 @@ public class Persistence {
    */
   public ArrayList<Client> loadClients() throws Exception {  
     JSONParser parser = new JSONParser();
-    JSONArray clientsArray = (JSONArray) parser.parse(new FileReader("C:\\Users\\Marcosmh0199\\Docume"
-        + "nts\\TEC\\2019\\PrimerSemestre\\POO\\Proyectos\\Proyecto1-Rent a Car\\obsidian-code\\"
-        + "rent-a-car\\clients.json"));
+    JSONArray clientsArray = (JSONArray) parser.parse(new FileReader("C:\\Users\\Marcosmh0199\\Doc"
+        + "uments\\TEC\\2019\\PrimerSemestre\\POO\\Proyectos\\Proyecto1-Rent a Car\\obsidian-code"
+        + "\\rent-a-car\\clients.json"));
     ArrayList<String> clientData= new ArrayList<String>();
     ArrayList<Client> clients = new ArrayList<Client>();
     for(int i = 0; i < clientsArray.size(); i++) {
@@ -124,5 +131,86 @@ public class Persistence {
       clients.add(client);
     }
     return clients;
+  }
+  
+  public void saveVehicle(Vehicle vehicle) {
+    ArrayList<String> vehicleData = new ArrayList<String>();
+    ArrayList<String> maintenanceData = new ArrayList<String>();
+    SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yy");
+    vehicleData.add(vehicle.getVehiclePlate());
+    vehicleData.add(formatDate.format(vehicle.getFabricationDate()));
+    vehicleData.add(vehicle.getColor().toString());
+    vehicleData.add(Byte.toString(vehicle.getCapacity()));
+    vehicleData.add(vehicle.getBrand());
+    vehicleData.add(Byte.toString(vehicle.getDoors()));
+    vehicleData.add(vehicle.getVinNumber());
+    vehicleData.add(Float.toString(vehicle.getMpg()));
+    vehicleData.add(Float.toString(vehicle.getPrice()));
+    vehicleData.add(Byte.toString(vehicle.getSuitcaseCapacity()));
+    vehicleData.add(Boolean.toString(vehicle.isTransmission()));
+    for(int i = 0; i < vehicle.getMaintenances().size(); i++) {
+      maintenanceData.add(Boolean.toString(vehicle.getMaintenances().get(i).getType()));
+      maintenanceData.add(vehicle.getMaintenances().get(i).getId());
+      maintenanceData.add(formatDate.format(vehicle.getMaintenances().get(i).getStartDate()));
+      maintenanceData.add(formatDate.format(vehicle.getMaintenances().get(i).getEndDate()));
+      maintenanceData.add(Float.toString(vehicle.getMaintenances().get(i).getPrice()));
+      maintenanceData.add(vehicle.getMaintenances().get(i).getDetail());
+    }
+    for(int i = 0; i < maintenanceData.size(); i++) {
+      vehicleData.add(maintenanceData.get(i));
+    }
+    try {
+      JSONParser parser = new JSONParser();
+      JSONArray vehicles = (JSONArray) parser.parse(new FileReader("C:\\Users\\Marcosmh0199\\Docume"
+          + "nts\\TEC\\2019\\PrimerSemestre\\POO\\Proyectos\\Proyecto1-Rent a Car\\obsidian-code\\"
+          + "rent-a-car\\vehicles.json"));
+      vehicles.add(vehicleData);
+      Files.write(Paths.get("vehicles.json"), vehicles.toJSONString().getBytes());
+    }catch(Exception d) {
+      JSONArray clients = new JSONArray();
+      clients.add(vehicleData);
+      Files.write(Paths.get("vehicles.json"), clients.toJSONString().getBytes());
+      }
+  }
+  
+  public ArrayList<Vehicle> loadVehicles() throws IOException, ParseException, 
+  java.text.ParseException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
+  SecurityException, ClassNotFoundException {
+    JSONParser parser = new JSONParser();
+    JSONArray vehiclesArray = (JSONArray) parser.parse(new FileReader("C:\\Users\\Marcosmh0199\\Docume"
+        + "nts\\TEC\\2019\\PrimerSemestre\\POO\\Proyectos\\Proyecto1-Rent a Car\\obsidian-code\\"
+        + "rent-a-car\\clients.json"));
+    ArrayList<String> vehicleData= new ArrayList<String>();
+    ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+    for(int i = 0; i < vehiclesArray.size(); i++) {
+      vehicleData = (ArrayList<String>) vehiclesArray.get(i);
+      final java.lang.reflect.Field field = Class.forName("java.awt.Color").getField(vehicleData.
+          get(2));
+      final String VEHICLEPLATE = vehicleData.get(0);
+      final Date FABRICATIONDATE = new SimpleDateFormat("dd/MM/yyyy").parse(vehicleData.get(1));
+      final Color COLOR = (Color)field.get(null);
+      final byte CAPACITY = Byte.parseByte(vehicleData.get(3));
+      final String BRAND = vehicleData.get(4);
+      final byte DOORS = Byte.parseByte(vehicleData.get(5));
+      final String VINNUMBER = vehicleData.get(6);
+      final float MPG = Float.parseFloat(vehicleData.get(7));
+      final float PRICE = Float.parseFloat(vehicleData.get(8));
+      final byte SUITCAPACITY = Byte.parseByte(vehicleData.get(9));
+      final boolean TRANSMISION = Boolean.parseBoolean(vehicleData.get(10));
+      Vehicle vehicle = new Vehicle(VEHICLEPLATE, FABRICATIONDATE, COLOR, CAPACITY, BRAND, DOORS, 
+          VINNUMBER, MPG, PRICE, SUITCAPACITY, TRANSMISION);
+      for(int j = 11; j < vehicleData.size(); j = j+6) {
+        final boolean TYPE = Boolean.parseBoolean(vehicleData.get(j));
+        final String ID = vehicleData.get(j+1);
+        final Date STARTDATE = new SimpleDateFormat("dd/MM/yyyy").parse(vehicleData.get(j+2));
+        final Date ENDDATE = new SimpleDateFormat("dd/MM/yyyy").parse(vehicleData.get(j+3));
+        final float MAITENANCEPRICE = Float.parseFloat(vehicleData.get(j+4));
+        final String DETAIL = vehicleData.get(j+5);
+        vehicle.getMaintenances().add(new Maintenance(TYPE, ID, STARTDATE, ENDDATE, MAITENANCEPRICE,
+            DETAIL));
+      }
+      vehicles.add(vehicle);
+    }
+    return vehicles;
   }
 }
