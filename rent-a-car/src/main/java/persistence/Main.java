5b3d073a47d4867755315b62062cd2168df3906d
Marcos Méndez Hidalgo <marcosmh0199@gmail.com>
2019-04-06 16:52:11 -0600

    feat: test & constants edit

--- rent-a-car/src/main/java/persistence/Main.java
+++ rent-a-car/src/main/java/persistence/Main.java
@@ -1,8 +1,9 @@
 package persistence;
 
-import java.awt.Color;
 import java.awt.Image;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -10,7 +11,9 @@ import javax.imageio.ImageIO;
 import bussineslogic.Address;
 import bussineslogic.Branch;
 import bussineslogic.Client;
+import bussineslogic.Company;
 import bussineslogic.Employee;
+import bussineslogic.Maintenance;
 import bussineslogic.Vehicle;
 
 /**
@@ -41,7 +44,7 @@ public class Main {
     
     clients= persistence.loadClients();
     for(int i = 0; i < clients.size(); i++) {
-      System.out.println(clients.get(i)+"\n\n");
+      //System.out.println(clients.get(i)+"\n\n");
     }
     
     Employee employee = new Employee("Marcos","117","8511","marcos@gmail");
@@ -52,33 +55,48 @@ public class Main {
     
     ArrayList<Employee> employees = persistence.loadEmployees();
     for(int i = 0; i < employees.size(); i++) {
-      System.out.println(employees.get(i)+"\n\n");
+      //System.out.println(employees.get(i)+"\n\n");
     }
-    
+    Branch branch1 = new Branch("Cartago",address);
+    Branch branch2 = new Branch("PEREZ",address);
+    Company company = new Company("HOLA", "COMO","ESTA");
     Vehicle vehicle = new Vehicle("12345", date1, "ROJO", (byte)4, "Nissan", (byte)2, "6789", (float)0, (float)230000.23, (byte)4, false);
     Vehicle vehicle2 = new Vehicle("12345", date1, "VERDE", (byte)4, "Toyota", (byte)2, "6789", (float)0, (float)230000.23, (byte)4, false);
+    vehicle.setBranch(branch1);
     vehicle.setVehicleImage(picture);
     vehicle2.setVehicleImage(picture);
-    
+    vehicle2.setBranch(branch2);
+    vehicle.getMaintenances().add(new Maintenance(true,vehicle.getVehiclePlate(),date1,date1, (float)5000, "Test", company));
     //persistence.saveVehicle(vehicle);
     //persistence.saveVehicle(vehicle2);
     
     ArrayList<Vehicle> vehicles = persistence.loadVehicles();
+    persistence.updateVehicleData(vehicles);
+    vehicles = persistence.loadVehicles();
     for(int i = 0; i < vehicles.size(); i++) {
-      System.out.println(vehicles.get(i).getVehicleImage().toString()+"\n\n");
+      System.out.println(vehicles.get(i).getBranch().getName()+"\n\n");
     }
-    
-    Branch branch1 = new Branch("Cartago",address);
-    Branch branch2 = new Branch("PEREZ",address);
-    branch1.setVehicles(vehicles);
-    branch2.setVehicles(vehicles);
+    //System.out.println(vehicles.get(0).getMaintenances().get(0).getCompany().getBussinesName());
+    branch1.add(vehicle);
+    branch2.add(vehicle2);
     
     //persistence.saveBranch(branch1);
     //persistence.saveBranch(branch2);
     ArrayList<Branch> branches = persistence.loadBranches();
-    
+    persistence.updateBranchVehicles(branches);
+
+    branches = persistence.loadBranches();
     for(int i = 0; i < branches.size(); i++) {
-      System.out.println(branches.get(i).getVehicles().get(i).getColor());
+      System.out.println(branches.get(i).getVehicles().get(0).getBranch().getName());
+    }
+    
+    //persistence.saveCompany(company);
+    //persistence.saveCompany(company);
+    //persistence.saveCompany(company);
+    
+    ArrayList<Company> companies = persistence.loadCompanies();
+    for(int i = 0; i < companies.size(); i++) {
+      System.out.println(companies.get(i).getBussinesName());
     }
   }
 
