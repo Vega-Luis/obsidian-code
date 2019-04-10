23e8e2e188997760d65be135ce3319c7f34dda39
Luis <vrl4@outlook.es>
2019-04-10 00:14:53 -0600

    feat: maps implementation

---
+++ rent-a-car/src/main/java/util/IMapConstants.java
@@ -0,0 +1,8 @@
+package util;
+
+public interface IMapConstants {
+  public final String HOST = "https://www.google.com/maps/dir/";
+  public final String HOST_END = ",10z/data=!4m2!4m1!3e0";
+  //https://www.google.com/maps/dir/9.8644499,-83.9194412/9.9333296,-84.0833282/@9.8644499,-83.9194412,10z/data=!4m2!4m1!3e0
+
+}
